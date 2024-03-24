6273b314e2be860bbc755e89d664c03e72902384
GEONU-MOON <moondy2209@naver.com>
2024-03-24 17:56:23 +0900

    #1260 #2606 #11724

---
+++ solve/2606.py
@@ -0,0 +1,22 @@
+import sys
+
+
+n=int(sys.stdin.readline())
+v=int(sys.stdin.readline()) 
+
+graph = [[] for i in range(n+1)]
+visited=[0]*(n+1) 
+
+def dfs(v):
+    visited[v]=1
+    for nx in graph[v]:
+        if visited[nx]==0:
+            dfs(nx)
+
+for i in range(v): 
+    a,b=map(int,input().split())
+    graph[a]+=[b] 
+    graph[b]+=[a] 
+
+dfs(1)
+print(sum(visited)-1)
