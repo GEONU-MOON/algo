ee817f368cd28225692150459a5b76721baa9b9b
GEONU-MOON <moondy2209@naver.com>
2024-03-26 01:46:34 +0900

    #2606 코드 보완

--- solve/2606.py
+++ solve/2606.py
@@ -1,22 +1,31 @@
 import sys
 
+input = sys.stdin.readline
 
-n=int(sys.stdin.readline())
-v=int(sys.stdin.readline()) 
+def dfs(idx) : 
+    global visited, graph, answer
+    visited[idx] = True
+    answer += 1
+    
+    for i in range(1, n + 1) :
+        if not visited[i] and graph[idx][i] :
+            dfs(i)
 
-graph = [[] for i in range(n+1)]
-visited=[0]*(n+1) 
+n = int(input())
+m = int(input())
 
-def dfs(v):
-    visited[v]=1
-    for nx in graph[v]:
-        if visited[nx]==0:
-            dfs(nx)
+graph = [[False] * (n + 1) for _ in range(n + 1)]
+visited = [False] * (n + 1)
+answer = 0
 
-for i in range(v): 
-    a,b=map(int,input().split())
-    graph[a]+=[b] 
-    graph[b]+=[a] 
-
+# 1. graph 연결 정보 채우기
+for _ in range(m) :
+    x, y = map(int, input().split())
+    graph[x][y] = True
+    graph[y][x] = True
+    
+# 2. dfs(재귀함수) 호출
 dfs(1)
-print(sum(visited)-1)
+
+# 3. 출력
+print(answer - 1)
