aa3cd06c7fded817cbcafbf1d2507452a044f174
GEONU-MOON <moondy2209@naver.com>
2024-03-31 20:20:59 +0900

    3/31

--- solve/11049.py
+++ solve/11049.py
@@ -8,7 +8,7 @@ D = [[-1 for j in range(N + 1)] for i in range(N + 1)]
 M.append((0, 0))
 for i in range(N):
     x, y = map(int, input().split())
-    M.append((x, y))
+    M.append((x, y)) 
 
 def excute(s, e):
     result = sys.maxsize
