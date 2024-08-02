53acbc0a2bc3e16d9ad300b0734b294501ea46dc
문건우 <121227401+GEONU-MOON@users.noreply.github.com>
2024-08-02 09:40:26 +0900

    [Silver V] Title: 배열 합치기, Time: 1568 ms, Memory: 188264 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -1,23 +1,6 @@
-n,m=map(int,input().split())
-A=list(map(int,input().split()))
-B=list(map(int,input().split()))
-ans=[]
-
-a=0
-b=0
-while a!=len(A) or b!=len(B):
-    if a==len(A): 
-        ans.append(B[b])
-        b+=1
-    elif b==len(B):
-        ans.append(A[a])
-        a+=1
-    else:
-        if A[a]<B[b]:
-            ans.append(A[a])
-            a+=1
-        else:
-            ans.append(B[b])
-            b+=1
-
-print(*ans)
+n, m = map(int,input().split())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+c = a+b
+c.sort()
+print(*c)
